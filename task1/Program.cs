7ef09e7b2612c52112ff34287240268892389a5b
nadezda staselovich <staselovichn@mail.ru>
2023-02-05 13:03:19 +0200

    add commit

---
+++ task1/Program.cs
@@ -0,0 +1,16 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+
+int FondCountPositiveNum(int[] arr)
+{
+  int count = 0;
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if(arr[i] > 0) count++;
+  }
+  return count;
+}
+
+System.Console.WriteLine(FondCountPositiveNum(arr));
